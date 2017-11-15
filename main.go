ab57961e25c95d5a3b9edbb3d6ebd49fef51d33f
rampancy2001 <33678764+rampancy2001@users.noreply.github.com>
2017-11-15 19:40:50 +0800

    Update main.go

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(replyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" 歡迎COC的朋友們!")).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.replyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" 歡迎COC的朋友們!")).Do(); err != nil {
 					log.Print(err)
 			
 }
