5df877c5d105c6809d9fad923d8f1fa056155db5
rampancy2001 <33678764+rampancy2001@users.noreply.github.com>
2017-11-16 20:45:21 +0800

    Update main.go

--- main.go
+++ main.go
@@ -97,7 +97,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				if strings.Contains(message.Text,"死我") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("RIP")).Do()}
 				if strings.Contains(message.Text,"幫忙捐兵") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("又來了,請大家幫忙捐兵喔")).Do()}
 				if strings.Contains(message.Text,"請問") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("請各位幫忙回答喔")).Do()}
-				if strings.Contains(message.Text,"測試") {bot.ReplyMessage(event.ReplyToken, linebot.NewImagemapMessage("http://www.wyattresearch.com/wp-content/uploads/2015/01/6_logo_predesign.jpg","http://www.wyattresearch.com/wp-content/uploads/2015/01/6_logo_predesign.jpg",linebot.ImagemapBaseSize{100, 100})).Do()}
+				if strings.Contains(message.Text,"測試") {bot.ReplyMessage(event.ReplyToken, "http://www.wyattresearch.com/wp-content/uploads/2015/01/6_logo_predesign.jpg").Do()}
 			}
 		}
 	}
