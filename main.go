fda221c6da0ef3a29e7bfc3a8c7a93c32ebee314
rampancy2001 <33678764+rampancy2001@users.noreply.github.com>
2017-12-08 17:14:20 +0800

    Update main.go

--- main.go
+++ main.go
@@ -155,7 +155,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				if strings.Contains(message.Text,"Ps4") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("誰有玩PS4 加個好友啦")).Do()}
 				if strings.Contains(message.Text,"ps4") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("誰有玩PS4 加個好友啦")).Do()}
 				if strings.Contains(message.Text,"野生") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("歐買尬!本人比較帥")).Do()}
-				if strings.Contains(message.Text,"媽的") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("人是人他媽生的，妖是妖他媽生的，只要你有一顆善良的心就不再是妖，是人妖？")).Do()}
+				if strings.Contains(message.Text,"媽的") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("人是人他媽生的，妖是妖他媽生的，只要你有一顆善良的心就不再是妖，是人妖")).Do()}
 				if strings.Contains(message.Text,"ㄇㄉ") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("你媽貴姓？")).Do()}
 				if strings.Contains(message.Text,"吃屎") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("又油又香 粉好吃")).Do()}
 				if strings.Contains(message.Text,"妹妹") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("醒醒吧~你沒有妹妹")).Do()}
@@ -228,6 +228,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				if strings.Contains(message.Text,"服了") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("I服了You")).Do()}
 				if strings.Contains(message.Text,"危險") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("你回火星吧，地球是很危險的")).Do()}
 				if strings.Contains(message.Text,"尊敬") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("我對你的敬仰真是如滔滔江水，連綿不絕，又有如黃河泛濫一發不可收拾。")).Do()}
+				if strings.Contains(message.Text,"自言自語") {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("還有我呢")).Do()}
 				if strings.Contains(message.Text,"笑話") {
 					a=rand.Intn(8)
 					if a == 0 {bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("有一個國家舉辦最噁心比賽，至最後決賽時剩下三個人爭冠，其中一個人拿了一杯尿，在嘴中漱了漱，吞下，哈的一聲，全場鼓掌想冠軍必落於此家。第二個人從袋中拿出一堆蟑螂，剝了翅膀就嘖嘖嘖的吸牠的肚子，不時還吐出一兩隻腳,吃到第十隻的時後,國王面有菜色的說不用吃了你這樣就第一名了。 此時見第三個人拿出一杯液體，說，這是我半年前感冒到現在，每次吐的痰都收集在裡面，我現在要把它喝完。只見一整杯白白黃黃還帶泡泡的，他搖了搖，試圖讓有些積太久快要凝固的化開， 國王眼淚都要掉下來了，說：不用了不用了你只要喝一口你就冠軍了～這人便拿起杯子咕嘟咕嘟地開始喝，因為很濃又很多過了五分多鐘才喝完，此時全場已淚流滿面，國王說幹嘛我不是叫你喝一口就冠軍了嗎？這人回答道，我也只是想喝一口，但是我一直咬不斷～～")).Do()}
